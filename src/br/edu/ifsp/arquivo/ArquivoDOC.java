92ca734b6b75c0a8882bfcee98d18946220f5b0b
Henrique Fernandes <henrique.contato@outlook.com>
2016-03-22 13:41:30 -0300

    Implementando Classe de ArquivoDOC

--- src/br/edu/ifsp/arquivo/ArquivoDOC.java
+++ src/br/edu/ifsp/arquivo/ArquivoDOC.java
@@ -1,10 +1,29 @@
 package br.edu.ifsp.arquivo;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import org.apache.poi.poifs.filesystem.POIFSFileSystem;
+import org.apache.poi.hwpf.extractor.WordExtractor;
+
 public class ArquivoDOC extends Arquivo {
 
 	@Override
 	public void lerArquivo() {
-		// TODO Auto-generated method stub
+		POIFSFileSystem sistemaDeArquivo = null;
+		WordExtractor extrator = null;  
+		String texto = "";
+		
+		try {
+			sistemaDeArquivo = new POIFSFileSystem(new FileInputStream(this.nome));
+			extrator = new WordExtractor(sistemaDeArquivo);  
+			texto = extrator.getText();
+			System.out.println(texto);
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 	}
 
--- src/br/edu/ifsp/arquivo/Main.java
+++ src/br/edu/ifsp/arquivo/Main.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Main {
 
 	public static void main(String[] args) throws IOException {
-		Arquivo a = Arquivo.criaArquivo("/home/henrique/Desktop/10.1.1.53.7394.pdf");
+		Arquivo a = Arquivo.criaArquivo("/home/henrique/Desktop/Exemplo de Plano de Testes.doc");
 		a.lerArquivo();
 	}
 }
