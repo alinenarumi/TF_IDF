4700c19714c1090048c35d5c17f8e02599509361
Henrique Fernandes <henrique.contato@outlook.com>
2016-03-31 12:16:05 -0300

    Renomeando classe Main para PontoDeEntrada

--- src/br/edu/ifsp/pontodeentrada/PontoDeEntrada.java
+++ src/br/edu/ifsp/pontodeentrada/PontoDeEntrada.java
@@ -4,10 +4,10 @@ import java.io.IOException;
 
 import br.edu.ifsp.arquivo.Arquivo;
 
-public class Main {
+public class PontoDeEntrada {
 
 	public static void main(String[] args) throws IOException {
-		Arquivo a = Arquivo.criaArquivo("/home/henrique/Desktop/ExercíciosJurosSimples-2.docx");
+		Arquivo a = Arquivo.criaArquivo("/home/henrique/Desktop/s.txt");
 		a.lerArquivo();
 	}
 }
